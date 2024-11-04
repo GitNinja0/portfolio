9822da6b126fb8b8ba8c4fa599d00d8adefc0a2c
Juan <146742892+GitNinja0@users.noreply.github.com>
2024-11-04 16:21:15 +0100

    create files Proyect.vue and proyects.ts, add code and modidy AboutMe.vue (delete image)

--- src/components/main/AboutMe.vue
+++ src/components/main/AboutMe.vue
@@ -1,11 +1,6 @@
 <template>
     <div class="about">
 
-        <div class="tittle_class">    
-            <h2>About Me</h2>
-            
-            <img src="./../../assets/images/_MG_8820.jpg" alt="Juan Image">
-        </div>
         <div class="text_class">
             <p> I am a passionate web application developer with experience in Front-End, Back-End, and Cloud. I have a basic level of English and stand out for being flexible, adaptive, and cooperative in team environments. In addition to my development skills, I have experience with WordPress and proficiency in design tools such as Adobe Photoshop, InDesign, Illustrator, and Figma. I work effectively with Agile methodologies like Scrum and have a solid knowledge of design patterns, specifically MVC. I am always willing to take on great challenges and am committed to continuous learning to improve my skills and contribute to the team's success
             </p>
@@ -23,25 +18,10 @@
 <style lang="scss" scoped>
 
 .about{
-    display: grid;
-    grid-template-columns: 1fr 2fr;
     margin: 1rem 0;
 
-    .tittle_class{
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        gap: 2rem;
-
-        img{
-            border-radius: 100%;
-            height: 18rem;
-            width: 18rem;
-        }
-    }
-
     .text_class{
-        margin-top: 2rem;
+        margin: 2rem;
         width: 95%;
     }
 }
---
+++ src/stores/proyects.ts
@@ -0,0 +1,22 @@
+import { defineStore } from "pinia";
+
+export const useProyectsStore = defineStore('proyect', ()=>{
+
+    const proyect= [
+        {
+            title: "Real State",
+            description: "",
+            type: "Front-End",
+            tecnologies: "Vue, JavaScript, Mail.js"
+        },
+        {
+            title: "PrintGO",
+            description: "",
+            type: "Front-End",
+            tecnologies: "Vue, JavaScript, Mail.js"
+        }
+
+    ]
+        
+        
+})
